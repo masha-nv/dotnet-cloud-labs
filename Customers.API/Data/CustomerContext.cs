40aa5daaee8141ff665a0776b0f6f93f60fb42b9
masha dev <mdevyatlovskaya@oasysic.com>
2026-01-29 20:25:10 -0500

    Jan29 add customer busket and basket item models

--- Customers.API/Data/CustomerContext.cs
+++ Customers.API/Data/CustomerContext.cs
@@ -8,4 +8,7 @@ public class CustomerContext(DbContextOptions<CustomerContext> options) : DbCont
 {
     public DbSet<Customer> Customers => Set<Customer>();
     public DbSet<CustomerAddress> CustomerAddresses => Set<CustomerAddress>();
+
+    public DbSet<CustomerBusket> Buskets => Set<CustomerBusket>();
+    public DbSet<BusketItem> Items => Set<BusketItem>();
 }
--- Customers.API/obj/Debug/net10.0/Customers.API.AssemblyInfo.cs
+++ Customers.API/obj/Debug/net10.0/Customers.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customers.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4d57591beedd23d47089dc8983c93a73937b75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efc2855311934f71b8d7a107a6a2150f915c3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customers.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customers.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
